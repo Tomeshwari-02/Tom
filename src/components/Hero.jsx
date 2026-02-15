466298cdb3d70c0f46e7fccab7bc94502dd40828
Tomeshwari Sahu <scketcha@gmail.com>
2026-02-15 22:42:27 +0530

    Update Hero component introduction text

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,8 +19,8 @@ const Hero = () => {
             Hi, I'm <span className='text-[#915EFF]'>Tomeshwari</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I develop 3D visuals, user <br className='sm:block hidden' />
-            interfaces and web applications
+            AI & Full-Stack Developer <br className='sm:block hidden' />
+            Open Source Contributor
           </p>
         </div>
       </div>
