e9f7e1ffb8136abf69a98b153b8557e58c9c174f
Tomeshwari Sahu <scketcha@gmail.com>
2026-02-18 17:37:03 +0530

    Revise About section with new personal introduction
    Updated personal description to reflect current status and skills.

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -47,11 +47,10 @@ const About = () => {
         variants={fadeIn("", "", 0.1, 1)}
         className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
       >
-        I'm a skilled software developer with experience in TypeScript and
-        JavaScript, and expertise in frameworks like React, Node.js, and
-        Three.js. I'm a quick learner and collaborate closely with clients to
-        create efficient, scalable, and user-friendly solutions that solve
-        real-world problems. Let's work together to bring your ideas to life!
+       I’m a passionate B.Tech student and aspiring AI & Full-Stack 
+        Developer with strong skills in Java, Python, and JavaScript. 
+        I have hands-on experience building applications using React, 
+        Node.js, and Three.js, along with core Java (OOP, DBMS concepts)!
       </motion.p>
 
       <div className='mt-20 flex flex-wrap gap-10'>
